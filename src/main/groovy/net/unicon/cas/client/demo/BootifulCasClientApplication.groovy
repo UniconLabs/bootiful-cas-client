69d4f21e2d424879a47a23d1effeb133a196648f
Dmitriy Kopylenko <dima767@gmail.com>
2020-06-22 16:08:26 -0400

    Finish up proxy authn sample

--- src/main/groovy/net/unicon/cas/client/demo/BootifulCasClientApplication.groovy
+++ src/main/groovy/net/unicon/cas/client/demo/BootifulCasClientApplication.groovy
@@ -2,11 +2,19 @@ package net.unicon.cas.client.demo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
 class BootifulCasClientApplication {
 
     static main(args) {
         SpringApplication.run BootifulCasClientApplication, args
+    }
+
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        builder.build()
     }
 }
--- src/main/groovy/net/unicon/cas/client/demo/MainController.groovy
+++ src/main/groovy/net/unicon/cas/client/demo/MainController.groovy
@@ -3,12 +3,14 @@ package net.unicon.cas.client.demo
 import org.jasig.cas.client.boot.configuration.EnableCasClient
 import org.jasig.cas.client.boot.configuration.CasClientConfigurer
 import org.jasig.cas.client.authentication.AttributePrincipal
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.client.RestTemplate
 
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -24,6 +26,9 @@ class MainController implements CasClientConfigurer {
         return casLogoutUrl
     }
 
+    @Autowired
+    RestTemplate restTemplate
+
     @RequestMapping(value = '/', method = RequestMethod.GET)
     def index(HttpServletRequest request, HttpServletResponse response) {
         'index'
@@ -32,9 +37,14 @@ class MainController implements CasClientConfigurer {
     @RequestMapping(value = '/protected', method = RequestMethod.GET)
     def protected1(HttpServletRequest request, Model model) {
         AttributePrincipal principal = request.userPrincipal as AttributePrincipal
-        String pt = principal.getProxyTicketFor("https://dk.example.org:8444/")
+        def pt = principal.getProxyTicketFor("https://dk.example.org:8444/data")
+        def urlEncodedPt = URLEncoder.encode(pt, 'UTF-8')
+        def respFromRestApi = restTemplate.getForObject("https://dk.example.org:8444/data?ticket=$urlEncodedPt", String)
         model.addAttribute('principal', principal)
-        model.addAttribute('PT', pt)
+        model.addAttribute('dataFromRestApi', respFromRestApi)
+
+
+
         'protected'
     }
 
